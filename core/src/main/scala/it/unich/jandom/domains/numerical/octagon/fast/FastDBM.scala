42decd8c6186364f952c6d8345c3464e7d0c52f9
WalkerTR <dal.bianco.davide@gmail.com>
2017-07-26 10:32:39 +0200

    FastDBM: Temporary implementation

--- core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
+++ core/src/main/scala/it/unich/jandom/domains/numerical/octagon/fast/FastDBM.scala
@@ -14,85 +14,181 @@ import scalaz.std.list._
 // quadratic worst case complexity, so we only perform it by piggybacking on
 // the closure operator. We also use closure computations as switching points.
 
-object CFDBMInstance {
-  def instance[M[_]]: DifferenceBoundMatrix[
-    ({ type T[S, A] = CFastDBM[M, S, A] })#T] = ???
-}
 
 // ADT of "closable" DBMs in their fast implementation from Vechev et al.
 // They are "closable" in the sense that they augment the ADT of fast DBMs with
 // the type-level capability of being indexed by their strong closure state.
-sealed trait CFastDBM[M[_], _, A]
-// Constructor of *closed* fast DBMs.
-case class CFast[M[_], A](m: FastDBM[M, A]) extends CFastDBM[M, Closed, A]
-// Constructor of *non-closed* fast DBMs.
-case class NCFast[M[_], A](m: FastDBM[M, A]) extends CFastDBM[M, NonClosed, A]
-case class TopFast[M[_], A](nOfVars: Int) extends CFastDBM[M, Closed, A]
-case class BottomFast[M[_], A](nOfVars: Int) extends CFastDBM[M, Closed, A]
 
-object Lol {
 
-  def nuffDecomposed(is: List[List[VarIndex]]): Boolean = ???
-  def nuffSparse(is: NNI): Boolean = ???
+class FastDBM[A](private val m: HalfMatrix[A],
+                 private val components: Set[Set[VarIndex]],
+                 val dimension: Int)
+                (implicit ifield: InfField[A]) {
 
-}
+  import VarIndexOps._
 
-sealed trait FastDBM[M[_], A]
+  val vars: Set[VarIndex] = components.foldRight[Set[VarIndex]](Set())(_ ++ _)
 
-// Full DBMs are fast DBMs that are not decomposed, i.e., they can be either
-// dense or sparse.
-// Sparsity details, including when to switch between dense and sparse
-// representation, is supposed to be handled by the specific implementation of
-// the the DenseSparse trait/typeclass.
-// An even better thing to do (time permitting) could be to use a suitable
-// abstract trait of DBMs that does not talk about sparsity at all (who cares
-// if the implementations use a dense/sparse representation anyway, as long as
-// they provide the needed DBM-like operations?)
-case class FullDBM[M[_], A](dbm: M[A], dsdbm: DenseSparseDBM[M]) extends FastDBM[M, A]
-
-// We store the independent components as a linked list of linked lists of
-// variable indices.
-// Independent components correspond to submatrices in the complete DBM,
-// and these can be dense or sparse. The exact sparsity is computed
-// on-the-fly before closure.
-
-// Octagon operators on the decomposed type:
-// Apart from closure (which is specialized according to sparsity), we apply the
-// standard operators independently on each submatrix.
-
-// NNI for decomposed DBMs is computed as the sum of its submatrices' NNI.
-case class DecomposedDBM[M[_], A](completeDBM: M[A],
-                                  indepComponents: Seq[Seq[VarIndex]],
-                                  rdbm: DenseSparseDBM[M]) extends FastDBM[M, A] {
-
-  // Skeleton of strong closure for decomposed matrices.
-  def decStrongClosure(m: DecomposedDBM[M, A])
-                      (implicit ifield: InfField[A])
-  : (CFastDBM[M, Closed, A], NNI, List[List[VarIndex]]) = {
-
-    val submatrices = indepComponents.map(seq => rdbm.extract(seq)(completeDBM))
-    Applicative[Option].sequence(
-      submatrices.map(m => rdbm.strongClosure(m)).toList) match {
-
-      case Some(closedSubs) => {
-        val (newMatrix, newNNI, newIndepComps) =
-          closedSubs.foldRight((completeDBM, 0, indepComponents))(
-            (x, y) => (x, y) match {
-              case ((sub, NNI(nni), _), (full, fullNNI, is)) =>
-                (rdbm.pour(sub)(full), nni + fullNNI, is)})
-        val actualNewIndepComponents = ???
-        val m =
-          if (Lol.nuffDecomposed(actualNewIndepComponents))
-            CFast(DecomposedDBM(newMatrix, actualNewIndepComponents, rdbm))
-          else
-            // might be that we have to compute the index of non-infinite terms
-            // during the strong closure above, and pass them to the dbm
-            // constructor.
-            CFast(FullDBM(newMatrix, rdbm))
-
-        (m, NNI(newNNI), actualNewIndepComponents)
-      }
-      case None => (BottomFast(rdbm.varIndices(completeDBM).length), ???, Nil)
-    }
+  def update(f: (Int, Int) => A): FastDBM[A] = {
+    val varIndeces = for (i <- 0 until dimension) yield VarIndex(i)
+    new FastDBM(m.update(f), Set(varIndeces.toSet), dimension)
   }
+
+  def get(i: Int, j: Int): A =
+    if (vars.contains(VarIndex(i/2)) && vars.contains(VarIndex(j/2))) {
+      m(i, j)
+    } else if (i == j) {
+      ifield.zero
+    } else {
+      ifield.infinity
+    }
+
+  def strongClosure: FastDBM[A] = ???
+  def incrementalClosure(v: VarIndex): FastDBM[A] = ???
+
+  def forget(vi: VarIndex): FastDBM[A] = {
+    val newComponents = components.map(_.filter(_ != vi))
+    new FastDBM(m, newComponents, dimension)
+  }
+
+  def flipVar(vi: VarIndex): FastDBM[A] =
+    if (vars.contains(vi)) {
+
+      var updateVars: Set[(VarIndex, VarIndex)] =
+        (vars.map((_, vi)) union (vars.map((vi, _))))
+        .filter({ case (v1, v2) => v1 <= v2 })
+
+      val indeces = varsToIndeces(updateVars)
+
+      val f: (Int, Int) => A = (i, j) => {
+        if (i == varPlus(vi) || i == varMinus(vi)) {
+          if (j == varPlus(vi) || j == varMinus(vi))
+            m.get(signed(i), signed(j))
+          else
+            m.get(signed(i), j)
+        } else {
+          if (j == varPlus(vi) || j == varMinus(vi))
+            m.get(i, signed(j))
+          else
+            m.get(i, j)
+        }
+      }
+
+      // apply f on m_iv and m_vj elements only, where
+      // i and j (and v) are used
+      val newM = indeces.foldLeft(m)((newM, idx) => {
+        val (i, j) = idx
+        newM.update(i, j, f(i, j))
+      })
+
+      new FastDBM(newM, components, dimension)
+    } else {
+      this
+    }
+
+  def addScalarOnVar(vi: VarIndex, const: A): FastDBM[A] = 
+    if (vars.contains(vi)) {
+
+      var updateVars: Set[(VarIndex, VarIndex)] =
+        (vars.map((_, vi)) union (vars.map((vi, _))))
+        .filter({ case (v1, v2) => v1 <= v2 })
+
+      val indeces = varsToIndeces(updateVars)
+
+      val f: (Int, Int) => A = (i, j) => {
+        val g1 = (i == varPlus(vi) && j != varPlus(vi) && j != varMinus(vi)) ||
+                 (j == varMinus(vi) && i != varPlus(vi) && i != varMinus(vi))
+        val g2 = (i != varPlus(vi) && i != varMinus(vi) && j == varPlus(vi)) ||
+                 (j != varPlus(vi) && j != varMinus(vi) && i == varMinus(vi))
+        val g3 = i == varPlus(vi) && j == varMinus(vi)
+        val g4 = i == varMinus(vi) && j == varPlus(vi)
+        if (g1) ifield.-(m.get(i, j), const) else
+        if (g2) ifield.+(m.get(i, j), const) else
+        if (g3) ifield.-(m.get(i, j), ifield.double(const)) else
+        if (g4) ifield.+(m.get(i, j), ifield.double(const)) else
+          m.get(i, j)
+      }
+
+      // apply f on m_iv and m_vj elements only, where
+      // i and j (and v) are used
+      val newM = indeces.foldLeft(m)((newM, idx) => {
+        val (i, j) = idx
+        newM.update(i, j, f(i, j))
+      })
+
+      new FastDBM(newM, components, dimension)
+    } else {
+      this
+    }
+
+
+  def dbmUnion(other: FastDBM[A]): FastDBM[A] = {
+    val newVars = vars intersect other.vars
+    val newComponents = components.map(_.filter(newVars.contains(_)))
+
+    val updateVars = for(i <- newVars; j <- newVars) yield (i, j)
+    val indeces = varsToIndeces(updateVars)
+
+    val f: (Int, Int) => A = (i, j) =>
+      ifield.max(m.get(i, j), other.m.get(i, j))
+
+    // apply f on m_iv and m_vj elements only, where
+    // i and j (and v) are used
+    val newM = indeces.foldLeft(m)((newM, idx) => {
+      val (i, j) = idx
+      newM.update(i, j, f(i, j))
+    })
+
+    new FastDBM(newM, newComponents, dimension)
+  }
+
+  def dbmIntersection(other: FastDBM[A]): FastDBM[A] = {
+    val newVars = vars union other.vars
+    val newComponents = Set(newVars)
+    
+    val updateVars = for(i <- newVars; j <- newVars) yield (i, j)
+    val indeces = varsToIndeces(updateVars)
+
+    val f: (Int, Int) => A = (i, j) =>
+      ifield.min(m.get(i, j), other.m.get(i, j))
+
+    // apply f on m_iv and m_vj elements only, where
+    // i and j (and v) are used
+    val newM = indeces.foldLeft(m)((newM, idx) => {
+      val (i, j) = idx
+      newM.update(i, j, f(i, j))
+    })
+
+    new FastDBM(newM, newComponents, dimension)
+  }
+
+  def widening(other: FastDBM[A]): FastDBM[A] = ???
+  def narrowing(other: FastDBM[A]): FastDBM[A] = ???
+
+  def isTop: Boolean = {
+    val checkVars = for(i <- vars; j <- vars; if i <= j) yield (i, j)
+    val indeces = varsToIndeces(checkVars)
+    indeces.forall({ case (i, j) =>
+      if (i == j) {
+        get(i, j) == ifield.zero
+      } else {
+        get(i, j) == ifield.infinity
+      }
+    })
+  }
+  def isBottom: Boolean = ???
+
+
+  // convert couples of variable indeces to couples of element indeces
+  // e.g. {(0,1)} to {(0,2), (0,3), (1,2), (1,3)}
+  private def varsToIndeces(variables: Set[(VarIndex, VarIndex)]) =
+    variables.flatMap({ case (v1, v2) =>
+      Set(
+        (varPlus(v1), varPlus(v2)),
+        (varPlus(v1), varMinus(v2)),
+        (varMinus(v1), varPlus(v2)),
+        (varMinus(v1), varMinus(v2))
+      )
+    })
+
 }
